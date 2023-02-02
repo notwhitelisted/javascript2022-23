1162c9d3d46dd6b90aaa2af406a3825284cc5895
aaron tang <98385199+notwhitelisted@users.noreply.github.com>
2023-02-01 23:25:05 -0800

    inheritance with object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -397,4 +397,31 @@ console.log(martha);
 //martha.introduction();
 martha.calcAge();
 
+/////////////////////////inheritance etween classes: object.create
+const PersonProto = {
+    calcAge() {
+        console.log(2023 - this.birthYear);
+    },
 
+    init(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }  
+}
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
