65bbf9b4b7e3ac86400b958e79ee7d95eba91330
Aaron Tang <iaarontang@gmail.com>
2023-02-02 18:53:35 -0800

    github fix

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -552,9 +552,67 @@ labelBalance.addEventListener('click', function() {
 // combinedData.forEach(doggyFunc);
 
 ////////////////////Coding Challenge #2
+const calcAverageHumanAge = function(ages) {
+  const humanAges = ages.map(age => age <= 2 ? 2 * age : 16 + age * 4);
+  console.log(humanAges);
+  
+  const adults = humanAges.filter(age => age >= 18)
+  console.log(humanAges);
+  console.log(adults);
 
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+  return average; 
+}
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
 
 ////////////////////Coding Challenge #3
-
+const calcAverageHumanAges = ages => ages
+.map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+.filter(age => age >= 18)
+.reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
 ////////////////////Coding Challenge #4
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+
+//a.
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+//console.log(dogs);
+
+//b.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+//console.log(dogSarah);
+console.log(`Sarah's dog is eating too ${dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'}`)
+
+//c.
+const ownersEatTooMuch = dogs.filter(dog => dog.curFood > dog.recFood).flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+const ownersEatTooLittle = dogs.filter(dog => dog.curFood < dog.recFood).flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+
+//d.
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`)
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`)
+
+//e. 
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+//f.
+const checkEatingOkay = (dogs.some(dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1));
+console.log(dogs.some(checkEatingOkay));
+
+//g.
+console.log(dogs.filter(checkEatingOkay));
+
+//h.
+//sort it by recommended food portion in an ascending order [1,2,3]
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -180,8 +180,136 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+//reveal sections
+const allSections = document.querySelectorAll('.section')
 
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+  //console.log(entry);
 
+  if (!entry.isIntersecting) {
+    return;
+  } else {
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  }
+} 
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+})
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
+
+//lazy loading images - performance
+const imgTargets = document.querySelectorAll('img[data-src]');
+//console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  //console.log(entry);
+  
+  if (!entry.isIntersecting) return;
+
+  //replace src w/ data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function() {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+})
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+//slider
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+
+  let currentSlide = 0;
+  const maxSlide = slides.length;
+
+  const createDots = function() {
+    slides.forEach(function (_, index) {
+      dotContainer.insertAdjacentHTML('beforeend', `<button class=dots__dot" data-slide="${index}"></button>`);
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function(slide) {
+    slides.forEach((s, index) => (s.style.transform = `translateX(${100 * (index - slide)}%)`));
+  };
+
+  //next slide 
+  const nextSlide = function () {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+
+    goToSlide(currentSlide);
+    activateDot(currentSlide);
+  };
+
+  const prevSlide = function() {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1;
+    } else {
+      currentSlide--;
+    }
+
+    goToSlide(currentSlide);
+    activateDot(currentSlide);
+  };
+
+  const init = function() {
+    goToSlide(0);
+    createDots();
+    activateDot(0);
+  };
+  init();
+
+  //event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    if (e.key === 'ArrowRight') nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
 
 
 
@@ -328,5 +456,4 @@ headerObserver.observe(header);
 // [...h1.parentElement.children].forEach(function (e) {
 //   if (e !== h1) e.style.transform = 'scale(0.5)';
 // })
-
 
