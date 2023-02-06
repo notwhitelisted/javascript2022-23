49ce40dd8a6fa7797a780abda3e19a44b780d972
Aaron Tang <iaarontang@gmail.com>
2023-02-05 23:39:17 -0800

    encapsulation + chaining.methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -477,49 +477,138 @@ jay.introduce();
 jay.calcAge();
 
 //////////////////Another class example
-class Account {
-    constructor(owner, currency, pin) {
-        this.owner = owner;
-        this.currency = currency;
-        this.pin = pin;
-        this.movements = [];
-        this.locale = navigator.language;
+// class Account {
+//     constructor(owner, currency, pin) {
+//         this.owner = owner;
+//         this.currency = currency;
+//         this.pin = pin;
+//         this.movements = [];
+//         this.locale = navigator.language;
         
-        console.log(`thanks for opening an account!, ${owner}`);
-    }
+//         console.log(`thanks for opening an account!, ${owner}`);
+//     }
 
-    //public interface
-    deposit(value) {
-        this.movements.push(value);
-    }
+//     //public interface
+//     deposit(value) {
+//         this.movements.push(value);
+//     }
 
-    withdraw(value) {
-        this.deposit(-value);
-    }
+//     withdraw(value) {
+//         this.deposit(-value);
+//     }
 
-    approveLoan(value) {
-        return true;
-    }
+//     approveLoan(value) {
+//         return true;
+//     }
 
-    requestLoan(value) {
-        if (this.approveLoan(value)) {
-            this.deposit(value);
-            console.log(`loan approved`);
-        }
-    }
-}
+//     requestLoan(value) {
+//         if (this.approveLoan(value)) {
+//             this.deposit(value);
+//             console.log(`loan approved`);
+//         }
+//     }
+// }
 
-const acc1 = new Account('aaron', 'usd', '1111');
-console.log(acc1);
+// const acc1 = new Account('aaron', 'usd', '1111');
+// console.log(acc1);
 
 // acc1.movements.push(250);
 // acc1.movements.push(-140);
 
-acc1.deposit(250);
-acc1.withdraw(140);
-acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+// acc1.deposit(250);
+// acc1.withdraw(140);
+// acc1.requestLoan(1000);
+// acc1.approveLoan(1000);
 
-console.log(acc1);
+// console.log(acc1);
 
-console.log(acc1.pin);
+// console.log(acc1.pin);
+
+
+// Encapsulation: Protected Properties and Methods
+// Encapsulation: Private Class Fields and Methods
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
+class Account {
+    // 1) Public fields (instances)
+    locale = navigator.language;
+  
+    // 2) Private fields (instances)
+    #movements = [];
+    #pin;
+  
+    constructor(owner, currency, pin) {
+      this.owner = owner;
+      this.currency = currency;
+      this.#pin = pin;
+  
+      // Protected property
+      // this._movements = [];
+      // this.locale = navigator.language;
+  
+      console.log(`Thanks for opening an account, ${owner}`);
+    }
+  
+    // 3) Public methods
+  
+    // Public interface
+    getMovements() {
+      return this.#movements;
+    }
+  
+    deposit(val) {
+      this.#movements.push(val);
+      return this;
+    }
+  
+    withdraw(val) {
+      this.deposit(-val);
+      return this;
+    }
+  
+    requestLoan(val) {
+      // if (this.#approveLoan(val)) {
+      if (this._approveLoan(val)) {
+        this.deposit(val);
+        console.log(`Loan approved`);
+        return this;
+      }
+    }
+  
+    static helper() {
+      console.log('Helper');
+    }
+  
+    // 4) Private methods
+    // #approveLoan(val) {
+    _approveLoan(val) {
+      return true;
+    }
+  }
+
+  const acc1 = new Account('aaron', 'EUR', 1111);
+
+  // acc1._movements.push(250);
+  // acc1._movements.push(-140);
+  // acc1.approveLoan(1000);
+  
+  acc1.deposit(250);
+  acc1.withdraw(140);
+  acc1.requestLoan(1000);
+  console.log(acc1.getMovements());
+  console.log(acc1);
+  Account.helper();
+  
+  // console.log(acc1.#movements);
+  // console.log(acc1.#pin);
+  // console.log(acc1.#approveLoan(100));
+  
+  // Chaining
+  acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+  console.log(acc1.getMovements());
+  //
