856e6856ec808e5e35b6d07c523f744653058e01
Aaron Tang <iaarontang@gmail.com>
2023-02-06 22:08:21 -0800

    geolocation

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,25 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//geolocation
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        const {latitude} = position.coords;
+        const {longitude} = position.coords;
+        console.log(`http://www.google.pt/maps/@${latitude}, ${longitude}`);
+
+        const coords = [latitude, longitude]
+        const map = L.map('map').setView(coords, 15);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+    }, function() {
+        alert('Could not get your position');
+    })
+}
