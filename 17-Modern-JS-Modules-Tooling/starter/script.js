6d6fdef3467331182c0ee4ee7b2c0e2b91c07c29
Aaron Tang <iaarontang@gmail.com>
2023-02-17 01:54:01 -0800

    more npm installations

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -52,3 +52,23 @@ console.log(lastPost2);
 
 
 /////////////////introduction to NPM
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+    cart: [
+        { product: 'bread', quantity: 5 },
+        { product: 'pizza', quantity: 5 },
+    ],
+    user : { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+///////////////////Bundling with Parcel and NPM scripts
+/* dev dependency - is a tool we need to build applications. no need to include in code.
+will appear as new field in json file. 
