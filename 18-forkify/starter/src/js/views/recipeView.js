4e3c81d9d31a9c0fcc1afc30358344c5c3d6b4bc
Aaron Tang <iaarontang@gmail.com>
2023-02-22 23:24:17 -0800

    changed ingredients layout

--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -1,7 +1,11 @@
+// import icons from '../img/icons.svg'; //parcel 1
+import icons from 'url:../../img/icons.svg'; //parcel 2
+// console.log(icons);
+
+import { Fraction } from 'fractional'
+// console.log(Fraction);
+
 export class RecipeView {
-    // import icons from '../img/icons.svg'; //parcel 1
-    import icons from 'url:../../img/icons.svg'; //parcel 2
-    // console.log(icons);
     #parentElement = document.querySelector('.recipe');
     #data;
     
@@ -84,20 +88,7 @@ export class RecipeView {
         <div class="recipe__ingredients">
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
-            ${this.#data.ingredients.map(ing => {
-                return `
-                <li class="recipe__ingredient">
-                    <svg class="recipe__icon">
-                    <use href=${icons}#icon-check"></use>
-                    </svg>
-                    <div class="recipe__quantity">${ing.quantity}</div>
-                    <div class="recipe__description">
-                    <span class="recipe__unit">${ing.unit}</span>
-                    ${ing.description}
-                    </div>
-                </li>
-                `;
-            }).join('')}
+            ${this.#data.ingredients.map(this.#generateMarkUpIngredient).join('')}
         </div>
 
         <div class="recipe__directions">
@@ -119,7 +110,22 @@ export class RecipeView {
             </a>
         </div>
     `;
-}
+    }
+
+    #generateMarkUpIngredient(ing) {
+        return `
+        <li class="recipe__ingredient">
+            <svg class="recipe__icon">
+            <use href=${icons}#icon-check"></use>
+            </svg>
+            <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+            <div class="recipe__description">
+            <span class="recipe__unit">${ing.unit}</span>
+            ${ing.description}
+            </div>
+        </li>
+        `;
+    }
 }
 
 export default new RecipeView();
