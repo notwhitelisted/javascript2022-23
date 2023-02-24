9cf9dfdea5d33068be5da69e1199e942877c5b34
Aaron Tang <iaarontang@gmail.com>
2023-02-24 01:29:29 -0800

    implemented temp error handling

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,5 +1,6 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js'
 
 export const state = {
     recipe: {},
@@ -7,12 +8,8 @@ export const state = {
 
 export const loadRecipe = async function(id) {
     try {
-        const res = await fetch(`${API_URL}/${id}`);    
-        //https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
-        const data = await res.json();
-    
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    
+        const data = await getJSON(`${API_URL}/${id}`)
+
         const { recipe } = data.data;
         state.recipe = {
           id:recipe.id,
@@ -26,6 +23,7 @@ export const loadRecipe = async function(id) {
         }
         console.log(state.recipe);
     } catch (err) {
-        console.error(err);
+        // throw err; 
+        console.error(`${err}`)
     }
 }
