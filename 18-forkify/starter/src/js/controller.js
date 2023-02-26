d37d68c2793bd799ab5e0eed23729d7b87b0e18d
Aaron Tang <iaarontang@gmail.com>
2023-02-26 03:55:43 -0800

    implementing search results

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -32,6 +32,27 @@ const controlRecipes = async function() {
   }
 }
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
 }
--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -28,3 +28,26 @@ export const loadRecipe = async function(id) {
         throw err; 
     }
 }
+
+export const loadSearchResults = async function (query) {
+    try {
+      state.search.query = query;
+  
+      const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
+      console.log(data);
+  
+      state.search.results = data.data.recipes.map(rec => {
+        return {
+          id: rec.id,
+          title: rec.title,
+          publisher: rec.publisher,
+          image: rec.image_url,
+          ...(rec.key && { key: rec.key }),
+        };
+      });
+      state.search.page = 1;
+    } catch (err) {
+      console.error(`${err}`);
+      throw err;
+    }
+  };
