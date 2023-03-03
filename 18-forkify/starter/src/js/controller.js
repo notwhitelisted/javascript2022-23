19232dc3ccb21554ff83b796a918807aebd6e08d
Aaron Tang <iaarontang@gmail.com>
2023-03-03 02:14:13 -0800

    uploading new recipe

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -96,12 +96,44 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('💥', err);
+    addRecipeView.renderError(err.message);
+  }
+};
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 
 }
 init();
--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -110,3 +110,39 @@ export const loadSearchResults = async function (query) {
   const clearBookmarks = function () {
     localStorage.clear('bookmarks');
   };
+
+  export const uploadRecipe = async function (newRecipe) {
+    try {
+      const ingredients = Object.entries(newRecipe)
+        .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
+        .map(ing => {
+          const ingArr = ing[1].split(',').map(el => el.trim());
+          // const ingArr = ing[1].replaceAll(' ', '').split(',');
+          if (ingArr.length !== 3)
+            throw new Error(
+              'Wrong ingredient fromat! Please use the correct format :)'
+            );
+  
+          const [quantity, unit, description] = ingArr;
+  
+          return { quantity: quantity ? +quantity : null, unit, description };
+        });
+  
+      const recipe = {
+        title: newRecipe.title,
+        source_url: newRecipe.sourceUrl,
+        image_url: newRecipe.image,
+        publisher: newRecipe.publisher,
+        cooking_time: +newRecipe.cookingTime,
+        servings: +newRecipe.servings,
+        ingredients,
+      };
+  
+      const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
+      state.recipe = createRecipeObject(data);
+      addBookmark(state.recipe);
+    } catch (err) {
+      throw err;
+    }
+  };
+  
